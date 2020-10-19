2204d682454b4e14a0280eda08bc62aa95d1fa45
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-10-19 16:27:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ app.get('/checkout.html', function(req, res){
 });
 
 app.post('/create-session', async (req, res) => {
+<<<<<<< HEAD
     switch (order_info.order_size){
         case 1:
             var session = await stripe.checkout.sessions.create({
@@ -296,6 +297,36 @@ app.post('/create-session', async (req, res) => {
             metadata: {'order_id': order_info.order_id},
             success_url: `${YOUR_DOMAIN}/success.html`,
             cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+=======
+    var local_price = order_price;
+    var local_desc = String(order_desc);
+    console.log('local_price is ' + local_price);
+    delete global[order_size];
+    delete global[order_price];
+    delete global[order_desc];
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+            {
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                name: 'EMA Online Store',
+                images: ['https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/121185484_10158652691288374_6371473402707957527_n.jpg?_nc_cat=111&_nc_sid=b9115d&_nc_ohc=s87FZ63TNKwAX9Dv8Ht&_nc_ht=scontent.fapa1-1.fna&oh=f6382a44ace51f3e269042529ba750b2&oe=5FAA9A15', 'https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/121239752_10158652691348374_2337616342705280587_n.jpg?_nc_cat=101&_nc_sid=b9115d&_nc_ohc=BRf6f4sxNccAX_lGh63&_nc_ht=scontent.fapa1-1.fna&oh=c5a4d7fdc585bb0c80c3d1677dafab61&oe=5FAB83B9'],
+                description: '2020 Hoodie',
+                metadata: {'order_id': order_id}
+                },
+                unit_amount: local_price,
+            },
+            quantity: 1,
+            description: local_desc,
+            },
+        ],
+        mode: 'payment',
+        metadata: {'order_id': order_id},
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+>>>>>>> parent of 5993768... Updating name for product
         });
         
         res.json({ id: session.id });

b9e0ae5b50b1b0cf4f768933bc142dd24b253bf2
Chandler de Spirlet <chandler.despirlet@icloud.com>
2020-10-20 17:02:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.post('/process_cart', function(req, res) {
     }
     */
     //req.session.order_desc = item_description;
-    res.render('checkout.html', {});
+    res.render('/checkout.html', {});
 });
 
 app.get('/checkout.html', function(req, res){

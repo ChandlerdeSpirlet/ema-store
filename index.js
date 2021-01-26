68a0c8aa2a6795e5954585ac7af783825c6e5a3e
Chandler de Spirlet <chandler.despirlet@icloud.com>
2021-01-25 19:50:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -102,14 +102,10 @@ router.post('/process_qty', function(req, res) {//towels
     req.session.qty_order_name = item.order_name;
     req.session.qty_order_email = item.order_email;
     req.session.qty_desc = '';
-    req.session.inventory_dec = '';
     if (item.blue_towel != 0){
         req.session.order_qty_size += 1;
-        if (req.session.inventory_dec == ''){
-            req.session.inventory_dec += 'blue_towel = blue_towel - ' + item.blue_towel;
-        } else {
-            req.session.inventory_dec += ', blue_towel = blue_towel - ' + item.blue_towel;
-        }
+        var dec_qty_blue = 'update inventory set qty = qty - $1 where product_id = $2';
+        db.none(dec_qty_blue, [item.blue_towel, 'blue_towel']);
         req.session.qty_order.push(['Hand Towel, Blue', Number(item.blue_towel), 1000]);
         if (req.session.qty_desc == ''){
             req.session.qty_desc += 'Hand Towel, Blue X ' + item.blue_towel;
@@ -119,11 +115,8 @@ router.post('/process_qty', function(req, res) {//towels
     }
     if (item.red_towel != 0){
         req.session.order_qty_size += 1;
-        if (req.session.inventory_dec == ''){
-            req.session.inventory_dec += 'red_towel = red_towel - ' + item.red_towel;
-        } else {
-            req.session.inventory_dec += ', red_towel = red_towel - ' + item.red_towel;
-        }
+        var dec_qty_red = 'update inventory set qty = qty - $1 where product_id = $2';
+        db.none(dec_qty_blue, [item.red_towel, 'red_towel']);
         req.session.qty_order.push(['Hand Towel, Red', Number(item.red_towel), 1000]);
         if (req.session.qty_desc == ''){
             req.session.qty_desc += 'Hand Towel, Red X ' + item.red_towel;
@@ -133,16 +126,9 @@ router.post('/process_qty', function(req, res) {//towels
     }
     req.session.qty_order_id = item.order_name.substring(0, 3).toLowerCase() + String(Math.floor(Math.random() * (1 + 10000 - 1)) + 1);
     const qty_query = 'insert into orders (order_id, order_name, email, pay_status, bill_total, order_contents) values ($1, $2, $3, $4, $5, $6);';
-    const dec_query = 'update inventory set $1;';
     db.query(qty_query, [req.session.qty_order_id, req.session.qty_order_name, req.session.qty_order_email, 'UNPAID', 0, req.session.qty_desc])
         .then(function(rows){
-            db.none(dec_query, [req.session.inventory_dec])
-                .then(function(rows){
-                    res.redirect('https://ema-store.herokuapp.com/qty_checkout.html');
-                })
-                .catch(function(err){
-                    console.log("Err in dec qty: " + err);
-                })
+            res.redirect('https://ema-store.herokuapp.com/qty_checkout.html');
         })
         .catch(function(err){
             console.log("Err in adding to db - qty: " + err);
